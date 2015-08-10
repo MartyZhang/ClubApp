94c50c9340430aa472a2b821e30534a6b2748c52
MartyZhang <yan.q.zhang@mail.mcgill.ca>
2015-08-09 20:24:32 -0400

    quit commit

--- app/src/main/java/martin/hksn/com/hksnapp2015/menu/MainMenuFragment.java
+++ app/src/main/java/martin/hksn/com/hksnapp2015/menu/MainMenuFragment.java
@@ -1,8 +1,17 @@
 package martin.hksn.com.hksnapp2015.menu;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by yanqzhang on 7/27/15.
  */
-public class MainMenuFragment {
-
+public class MainMenuFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
 }
